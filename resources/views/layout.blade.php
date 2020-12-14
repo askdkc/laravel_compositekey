36e105398a1a7fdb2d4aff67152e81887fb6b10a
dkc <askdkc@users.noreply.github.com>
2020-12-14 22:01:40 +0900

    少しTailwindcss化

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Laravel</title>
-	{{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css"> --}}
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-	<style type="text/css">
-		.is-complete {
-			text-decoration: line-through;
-		}
-	</style>
+	<title>Laravel</title>
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+	<!-- ここtailwindcss化が終わったらあとで消す -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<!-- ここtailwindcss化が終わったらあとで消す -->
+
 </head>
 <body>
 	<div class="container">
--- resources/views/memos/index.blade.php
+++ resources/views/memos/index.blade.php
@@ -1,33 +1,64 @@
 @extends('layout')
 @section('content')
 
- <div class="row justify-content-left">
-
-<div class="uper">
+<div class="container mx-auto p-4">
   @if(session()->get('success'))
     <div class="alert alert-success">
       {{ session()->get('success') }}
     </div><br />
   @endif
-    <a href="/create"><button type="button" class="btn btn-primary mt-3">新規データ作成</button></a><br><br>
-  <table class="table table-striped">
-    <thead>
-        <tr>
-          <td>ID</td>
-          <td>独自ID</td>
-          <td>メモ</td>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($memos as $memo)
-        <tr>
-            <td>{{$memo->id}}</td>
-            <td>{{$memo->subid}}</td>
-            <td><a href="/memos/{{ $memo->id }}/{{ $memo->subid }}/edit/">{{$memo->memo}}</a></td>
-        </tr>
-        @endforeach
-    </tbody>
-  </table>
-<div>
+    <a href="/create"><button type="submit" class="inline-flex justify-center py-2 px-4 mb-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">新規データ作成</button></a>
+
+
+    <div class="flex flex-col">
+      <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-200">
+              <thead class="bg-gray-50">
+                <tr>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    ID
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    独自ID
+                  </th>
+                  <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    メモ
+                  </th>
+                  <th scope="col" class="relative px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Edit
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="bg-white divide-y divide-gray-200">
+                @foreach($memos as $memo)
+                <tr>
+                  <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="flex items-center">
+                      <div class="ml-4">
+                        <div class="text-sm font-medium text-gray-900">{{$memo->id}}</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap">
+                    <div class="text-sm text-gray-900">{{$memo->subid}}</div>
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                   <div class="text-sm text-gray-900">{{$memo->memo}}</div>
+                  </td>
+                  <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                    <a href="/memos/{{ $memo->id }}/{{ $memo->subid }}/edit/">Edit</a>
+                  </td>
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
 
 @endsection
